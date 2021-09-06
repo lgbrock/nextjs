7fd76774287cdf4e258c1cee956b9a8dc55a97e3
Logan Brock <logan.g.brock@gmail.com>
2021-09-06 16:45:22 -0400

    initial commit

---
+++ components/Layout.js
@@ -0,0 +1,21 @@
+import Navbar from './Navbar';
+import Header from './Header';
+import Meta from './Meta';
+import styles from '../styles/Layout.module.css';
+
+const Layout = ({ children }) => {
+	return (
+		<>
+			<Meta />
+			<Navbar />
+			<div className={styles.container}>
+				<main className={styles.main}>
+					<Header />
+					{children}
+				</main>
+			</div>
+		</>
+	);
+};
+
+export default Layout;
